be29243fb26f7337415de402cba67ad8aefe0434
allay <suraceallay@gmail.com>
2025-11-06 16:52:44 +0600

    pull

---
+++ src/content/about-us/AboutUs.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "layouts/MainLayout";
+
+const AboutUs: React.FC = () => {
+  return (
+    <MainLayout>
+      <div>About us</div>
+    </MainLayout>
+  );
+};
+
+export default AboutUs;
