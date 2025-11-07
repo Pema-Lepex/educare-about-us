9598c4fda2899eccf522fafa47dcaf3357354440
allay <suraceallay@gmail.com>
2025-11-07 10:09:27 +0600

    team

---
+++ src/assets/content/team/temInfo.tsx
@@ -0,0 +1,12 @@
+export const AboutContent1: React.FC = () => {
+  return (
+    <>
+        <h2>
+            Our amazing team
+        </h2>
+      <p>
+        We believe that a company makes people and its people completes the company. A company and people are inseparable! At iBEST. We believe our company is what values our people uphold and live.
+      </p>
+    </>
+  );
+};
