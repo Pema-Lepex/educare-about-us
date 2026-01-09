e7936c9af926690f3a9ff0156b71b895276b745c
Your Name <you@example.com>
2026-01-09 11:17:23 +0600

    final chnage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -8,7 +8,6 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import Impact from "modules/impact/Impact";
 import WhyChooseUs from "modules/why-choose-us/WhyChooseUs";
 import Leadership from "modules/leadership/Leadership";
-import Team from "modules/team/Team";
 import Blog from "modules/blog/Blog";
 import Career from "modules/career/Career";
 import Contact from "modules/contact/Contact";
@@ -45,7 +44,6 @@ const AppRoutes: React.FC = () => {
          {/* Why Choose Us page routes */}
         <Route path="/why-choose-us" element={<WhyChooseUs />} />
         <Route path="/leadership" element={<Leadership />} />
-        <Route path="/team" element={<Team />} />
         <Route path="/career" element={<Career />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
