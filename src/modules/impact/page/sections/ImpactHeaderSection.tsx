c28aa3581e124b0cf8ab1defe5ffcec74352a13f
Your Name <you@example.com>
2026-01-10 20:34:01 +0600

    dfgh

--- src/modules/impact/page/sections/ImpactHeaderSection.tsx
+++ src/modules/impact/page/sections/ImpactHeaderSection.tsx
@@ -1,35 +1,43 @@
-import { HMBGImage, HMK5Image, HMBGMobileImage, K5Image, K5PhoneImage } from "assets";
+import {
+  HMBGImage,
+  HMK5Image,
+  HMBGMobileImage,
+  K5Image,
+  K5PhoneImage,
+} from "assets";
 
 const ImpactHeaderSection: React.FC = () => {
   return (
     <section>
-  <div className="relative w-full">
-    <img
-      src={K5Image}
-      alt="background"
-      className="w-full lg:flex hidden h-auto min-h-[300px] object-cover bg-blue-400/90"
-    />
-    <img
-      src={K5PhoneImage}
-      alt="background"
-      className="w-full lg:hidden h-auto min-h-[300px] object-cover bg-blue-400/90"
-    />
-    <div className="absolute inset-0 flex mt-7 lg:mt-0 lg:items-center justify-end px-6 sm:px-12 md:px-24 lg:px-32">
-      <div className="w-2/3 md:w-3/5 lg:w-2/5 text-left">
-        <p className="text-[10px] sm:text-base md:text-base lg:text-lg xl:text-2xl 2xl:text-3xl 3xl:text-4xl 4xl:text-5xl 5xl:text-6xl text-white mb-2 sm:mb-4 leading-tight px-5 lg:px-0">
-          We need to be prepared for the future so that every one of us is
-          equipped with the skills, competencies and experience to succeed
-          and compete in the world. We must give utmost priority to skill,
-          up-skill, and re-skill our people to make them world-ready.
-        </p>
-        <p className="text-[10px] sm:text-base md:text-base xl:text-2xl 2xl:text-3xl 3xl:text-4xl 4xl:text-5xl 5xl:text-6xl font-bold text-yellow-400 px-5 lg:px-0">
-          - His Majesty The King of Bhutan, 2021 National Day Royal Address
-        </p>
+      <div className="relative w-full">
+        <img
+          src={K5Image}
+          alt="background"
+          className="w-full lg:flex hidden h-auto min-h-[300px] object-cover bg-blue-400/90"
+        />
+        <img
+          src={K5PhoneImage}
+          alt="background"
+          className="w-full lg:hidden h-auto min-h-[300px] object-cover bg-blue-400/90"
+        />
+        <div className="absolute inset-0 flex mt-7 lg:mt-0 lg:items-center justify-end px-6 sm:px-12 md:px-24 lg:px-32">
+          <div className="w-2/3 md:w-3/5 lg:w-2/5 text-left">
+            <p className="text-[10px] sm:text-base md:text-base lg:text-lg xl:text-2xl 2xl:text-3xl 3xl:text-4xl 4xl:text-5xl 5xl:text-6xl text-white mb-2 sm:mb-4 leading-tight px-5 lg:px-0">
+              We need to be prepared for the future so that every one of us is
+              equipped with the skills, competencies and experience to succeed
+              and compete in the world. We must give utmost priority to skill,
+              up-skill, and re-skill our people to make them world-ready.
+            </p>
+            <p className="flex gap-2 text-[10px] sm:text-base md:text-base xl:text-2xl 2xl:text-3xl 3xl:text-4xl 4xl:text-5xl 5xl:text-6xl font-bold text-yellow-400">
+              <span>-</span>
+              <span>
+                His Majesty The King of Bhutan, 2021 National Day Royal Address
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
-      
-    </div>
-  </div>
-</section>
+    </section>
   );
 };
 
