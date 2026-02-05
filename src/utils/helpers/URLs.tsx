4c52fd937b30d2837b9051711205a770004e286b
Your Name <you@example.com>
2026-02-05 14:54:31 +0600

    sdfghj

--- src/utils/helpers/URLs.tsx
+++ src/utils/helpers/URLs.tsx
@@ -1,31 +1,31 @@
 export const GiftCouponLinkDetails = {
   LinkFor: "Gift Coupon",
-  linkTo: "https://educareskill.com/?tab=gift-coupon",
+  linkTo: "http://localhost:8090/?tab=gift-coupon",
 };
 
 export const GiftCouponAuthenticatedLinkDetails = {
   LinkFor: "Gift Coupon",
-  linkTo: "https://educareskill.com/gift-coupon",
+  linkTo: "http://localhost:8090/gift-coupon",
 };
 
 export const DCDDSignUpLinkDetails = {
   LinkFor: "DCDD Sign In",
-  linkTo: "https://educareskill.com/dcdd-login",
+  linkTo: "http://localhost:8090/dcdd-login",
 };
 
 export const SignInLinkDetails = {
   LinkFor: "Sign In",
-  linkTo: "https://educareskill.com/login",
+  linkTo: "http://localhost:8090/login",
 };
 
 export const RedirectToAppStore = {
   LinkFor: "Redirect to App Store",
-  linkTo: "https://www.apple.com/app-store/",
+  linkTo: "https://apps.apple.com/us/app/educare-skill/id6479234341",
 };
 
 export const RedirectToGooglePlay = {
   LinkFor: "Redirect to Google Play",
-  linkTo: "https://play.google.com/store",
+  linkTo: "https://play.google.com/store/apps/details?id=com.skill.educare&hl=en",
 }; 
 
 export const ApplicationFormLinkDetails = {
@@ -35,12 +35,12 @@ export const ApplicationFormLinkDetails = {
 
 export const UserProfileLinkDetails = {
   LinkFor: "User Profile",
-  linkTo: "https://educareskill.com/profile",
+  linkTo: "http://localhost:8090/profile",
 };
 
 export const Home = {
   LinkFor: "Home",
-  linkTo: "https://educareskill.com",
+  linkTo: "http://localhost:8090",
 };
 
 export const Facebook = {
@@ -78,3 +78,83 @@ export const DCDDWebSiteLink = {
     LinkFor: 'DCDD',
     linkTo: 'https://doc.gov.bt'
 };
+// export const GiftCouponLinkDetails = {
+//   LinkFor: "Gift Coupon",
+//   linkTo: "https://educareskill.com/?tab=gift-coupon",
+// };
+
+// export const GiftCouponAuthenticatedLinkDetails = {
+//   LinkFor: "Gift Coupon",
+//   linkTo: "https://educareskill.com/gift-coupon",
+// };
+
+// export const DCDDSignUpLinkDetails = {
+//   LinkFor: "DCDD Sign In",
+//   linkTo: "https://educareskill.com/dcdd-login",
+// };
+
+// export const SignInLinkDetails = {
+//   LinkFor: "Sign In",
+//   linkTo: "https://educareskill.com/login",
+// };
+
+// export const RedirectToAppStore = {
+//   LinkFor: "Redirect to App Store",
+//   linkTo: "https://apps.apple.com/us/app/educare-skill/id6479234341",
+// };
+
+// export const RedirectToGooglePlay = {
+//   LinkFor: "Redirect to Google Play",
+//   linkTo: "https://play.google.com/store/apps/details?id=com.skill.educare&hl=en",
+// }; 
+
+// export const ApplicationFormLinkDetails = {
+//   LinkFor: "Submit Application",
+//   linkTo: "https://forms.gle/3aF7nE6MU1ofzKdj7",
+// };
+
+// export const UserProfileLinkDetails = {
+//   LinkFor: "User Profile",
+//   linkTo: "https://educareskill.com/profile",
+// };
+
+// export const Home = {
+//   LinkFor: "Home",
+//   linkTo: "https://educareskill.com",
+// };
+
+// export const Facebook = {
+//     LinkFor: 'Facebook',
+//     linkTo: 'https://www.facebook.com/educareskill'
+// };
+// export const Instagram = {
+//     LinkFor: 'Instagram',
+//     linkTo: 'https://www.instagram.com/educare.skill'
+// };
+// export const Youtube = {
+//     LinkFor: 'YouTube',
+//     linkTo: 'https://www.youtube.com/@educareskill'
+// };
+// export const Tiktok = {
+//     LinkFor: 'TikTok',
+//     linkTo: 'https://www.tiktok.com/@educare.skill'
+// };
+
+// export const IBESTSTUDIOS = {
+//     LinkFor: 'iBEST STUDIOS',
+//     linkTo: 'https://www.ibeststudios.com'
+// };
+
+// export const IBESTTECHNOLOGIES = {
+//     LinkFor: 'TECHNOLOGIES',
+//     linkTo: 'https://www.ibesttechnologies.bt'
+// };
+// export const MoEWebSiteLink = {
+//     LinkFor: 'MoE',
+//     linkTo: 'https://education.gov.bt'
+// };
+
+// export const DCDDWebSiteLink = {
+//     LinkFor: 'DCDD',
+//     linkTo: 'https://doc.gov.bt'
+// };
