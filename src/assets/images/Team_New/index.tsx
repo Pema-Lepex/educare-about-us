1e330907f963b5a3b82c7ef5ceb170bcee1d5b2d
Your Name <you@example.com>
2026-03-03 10:21:30 +0600

    mission and team

--- src/assets/images/Team_New/index.tsx
+++ src/assets/images/Team_New/index.tsx
@@ -1,7 +1,7 @@
 export { default as CEOProfile } from "./Sir T.png";
 export { default as CoFounderProfile } from "./Sir S.png";
 export { default as NewSoftwareDeveloperTeam} from "./Software_Developers.jpg"
-export { default as NewManagementTeam} from "./Management_Team.jpg"
+export { default as NewManagementTeam} from "./Management Team.jpg"
 export { default as NewProductionTeam} from "./Content_Writers.jpg"
 export { default as NewIllustratorsProfile} from "./Illustrators.jpg"
 export { default as NewThreeDAnimatorProfile} from "./3D_Animator.jpg"
