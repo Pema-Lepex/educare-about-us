b58185c16a04af92deb4f041c5d4360aa7e2b835
Your Name <you@example.com>
2026-03-06 11:17:51 +0600

    images

--- src/assets/images/Box/index.tsx
+++ src/assets/images/Box/index.tsx
@@ -1,4 +1,4 @@
-export {default as PurpleBoxImage} from "./1.png";
-export {default as RedBoxImage} from "./2.png";
-export {default as BlueBoxImage} from "./3.png";
-export {default as GreenBoxImage} from "./4.png";
+export {default as PurpleBoxImage} from "./Purple.png";
+export {default as RedBoxImage} from "./Red.png";
+export {default as BlueBoxImage} from "./Blue.png";
+export {default as GreenBoxImage} from "./Green.png";
